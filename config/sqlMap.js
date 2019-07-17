0a082920684132989381dba7b38eab1fc86009dd
yang47 <15228782447@163.com>
2019-07-17 17:40:26 +0800

    update

--- config/sqlMap.js
+++ config/sqlMap.js
@@ -1,25 +1,29 @@
 let sql = {
-    user:{
+    user: {
         //注册
-        regUser:'insert into user(uid,username,password) values(0,?,?)',
+        regUser: 'insert into user(uid,username,password) values(0,?,?)',
         //查询用户
-        query:'select * from user',
+        query: 'select * from user',
         //登录
-        login:'select * from user where username = ? and password = ?',
+        login: 'select * from user where username = ? and password = ?',
         //查询用户角色
-        queryRoles:'select rolesname from userroles where uid = ?',
+        queryRoles: 'select rolesname from userroles where uid = ?',
         //查询用户信息
-        queryInfo:'select * from userinfo where uid = ?'
+        queryInfo: 'select * from userinfo where uid = ?'
     },
-    articles:{
+    articles: {
         //查询所有分类
-    	queryCatalogs:"select * from catalog",
+        queryCatalogs: "select * from catalog",
         //新建标签
-        addTags:"insert into tags(id,name) values(0,?)",
+        addTags: "insert into tags(id,name) values(0,?)",
         //新建文章
-        createArticles:"insert into article(aid,title,author,cid,content,createDate) values(0,?,?,?,?,now())",
-
-     }
+        createArticles: "insert into article(aid,title,author,cid,content,createDate) values(0,?,?,?,?,now())",
+        //删除文章
+        delete: "delete from article where aid = ? ",
+        //根据id查询文章
+        queryForId: "select * from article where aid = ?",
+        //得到总页数
+        queryNum: "SELECT COUNT(*) FROM table_name"
+    }
 }
-
 module.exports = sql;
--- routes/articles.js
+++ routes/articles.js
@@ -5,80 +5,95 @@ const db = require('../config/db.js')
 const $sql = require('../config/sqlMap.js')
 let conn = mysql.createConnection(db.mysql)
 let Token = require('../config/token.js')
-
 // 定义一个返回变量的格式
 let resData;
 router.use((req, res, next) => {
-	resData = {
-		code: 'ok',
-		message: ''
-	}
-	next()
+    resData = {
+        code: 'ok',
+        message: ''
+    }
+    next()
 })
-
-
 //得到分类
-router.get('/getCatalogs',(req,res)=>{
-	let sql = $sql.articles.queryCatalogs
-	conn.query(sql, (err, result) => {
-		if (err) {
-			console.log(err)
-		}
-		if (result) {
-			console.log(result);
-			if(result.length>0){
-				resData.data = result;
-			}else{
-				resData.message ="暂时没有分类"
-			}
-			res.json(resData)
-		}
-	})
+router.get('/getCatalogs', (req, res) => {
+    let sql = $sql.articles.queryCatalogs
+    conn.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        if (result) {
+            console.log(result);
+            if (result.length > 0) {
+                resData.data = result;
+            } else {
+                resData.message = "暂时没有分类"
+            }
+            res.json(resData)
+        }
+    })
 })
-
 //查询文章列表(未分页)
-router.get('/getList',(req,res)=>{
-	let sql = $sql.articles.queryArticles
-	conn.query(sql, (err, result) => {
-		if (err) {
-			console.log(err)
-		}
-		if (result) {
-			if(result.length>0){
-				resData.data = result;
-			}else{
-				resData.message ="暂时没有文章"
-			}
-			res.json(resData)
-		}
-	})
+router.get('/getList', (req, res) => {
+    let currentPage = req.query.currentPage || 1;
+    let pageSize = req.query.pageSize || 10
+
+    let sql =  'SELECT * FROM article limit ' + pageSize + ' offset ' + pageSize * (currentPage - 1);
+    // let sql1 = 'SELECT found_rows() AS rowcount;'
+    conn.query(sql, [currentPage, pageSize], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        if (result) {
+            console.log(result)
+            if (result.length > 0) {
+                resData.data = result;
+            } else {
+                resData.code = 'failed'
+                resData.message = '没有更多数据了'
+            }
+            res.json(resData)
+        }
+    })
 })
-
-
 //新建文章
-router.post('/create',(req,res)=>{
-	let sql = $sql.articles.createArticles;
-	let params = req.body;
-	conn.query(sql,[params.title,params.author,params.cid,params.content,params.createDate] ,(err, result) => {
-		if (err) {
-			console.log(err)
-		}
-		if (result) {
-			console.log(result);
-			let aid = result.insertId;
-			resData.data = {'aid':aid}
-			resData.message = "创建成功"
-			res.json(resData)
-		}
-	})
+router.post('/create', (req, res) => {
+    let sql = $sql.articles.createArticles;
+    let params = req.body;
+    conn.query(sql, [params.title, params.author, params.cid, params.content, params.createDate], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        if (result) {
+            console.log(result);
+            let aid = result.insertId;
+            resData.data = {
+                'aid': aid
+            }
+            resData.message = "创建成功"
+            res.json(resData)
+        }
+    })
 })
-
 //删除文章
-
 //修改文章
-
-
-
-
-
+//根据id得到文章
+router.get('/getArticles', (req, res) => {
+    let sql =  $sql.articles.queryForId
+    let aid = req.query.aid
+    conn.query(sql, [aid], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        if (result) {
+            console.log(result)
+            if (result.length > 0) {
+                resData.data = result[0];
+            } else {
+                resData.code = 'failed'
+                resData.message = '没有更多数据了'
+            }
+            res.json(resData)
+        }
+    })
+})
 module.exports = router;
