891f9b51c5eeec1723be64d83805c839fb37579d
yang47 <15228782447@163.com>
2019-08-12 17:22:59 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -90,7 +90,6 @@ router.get('/getInfo',(req,res)=>{
 			console.log(result);
 			resData.message = "查询成功"
 			let userInfo = result[0];
-			userInfo.birthday = new Date(userInfo.birthday);
 			resData.data = userInfo
 			res.json(resData)
 		}
