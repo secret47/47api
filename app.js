7663822c2b0110783835ac560d883618bb1f41f6
yang47 <15228782447@163.com>
2019-08-29 14:07:24 +0800

    update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const sinaCloud = require('scs-sdk')
 //解决跨域请求
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,token");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,token,Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
